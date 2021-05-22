02be92555b05b81c75cd9875ca28ccda191ca18e
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-05-22 12:56:33 +0200

    Udaje o ceste

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { JourneyPicker } from './JourneyPicker';
 
 export const Home = () => {
-  return <h2>Home</h2>;
+  return <JourneyPicker />;
 };
--- src/Reservation/index.jsx
+++ src/Reservation/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 export const Reservation = () => {
   return <h2>Vaše jízdenka</h2>;
